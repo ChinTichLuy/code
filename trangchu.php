ca1a646d0dea103cbd35533dfaaf2610108e888c
HasterIT <dp73287@gmail.com>
2024-06-02 13:58:32 +0700

    mới update được cái nút tìm kiếm

--- trangchu.php
+++ trangchu.php
@@ -18,16 +18,25 @@
             <svg width="250" height="80">
                 <rect x="0" y="0" width="150" height="80" fill="#234BBB"></rect>
                 <text x="45" y="50" fill="#fdd602" font-size="30" font-weight="700">ChinPET.vn</text>
-                <div>
-                    <input type="text">
-                    <button>Tìm Kiếm</button>
-                </div>
-                <div>
-                    <Button>Đăng nhập</Button>
-                </div>
-                <div>
-                    <Button>Giỏ hàng</Button>
-                </div>
+            </svg>
+            <form action="" method="POST">
+                <input type="text" name="search">
+                <button class="btn_search" aria-label="Tìm kiếm">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="icon-search">
+                        <path
+                            d="M495,466.2L377.2,348.4c29.2-35.6,46.8-81.2,46.8-130.9C424,103.5,331.5,11,217.5,11C103.4,11,11,103.5,11,217.5  
+                             S103.4,424,217.5,424c49.7,0,95.2-17.5,130.8-46.7L466.1,495c8,8,20.9,8,28.9,0C503,487.1,503,474.1,495,466.2z M217.5,382.9   
+                             C126.2,382.9,52,308.7,52,217.5S126.2,52,217.5,52C308.7,52,383,126.3,383,217.5S308.7,382.9,217.5,382.9z">
+                        </path>
+                    </svg>
+                </button>
+            </form>
+            <div class="head_right">
+                <div class="hotline"></div>
+                <div class="favorite"></div>
+                <div class="login_header"></div>
+                <div class="cart_header"></div>
+            </div>
         </header>
         <section class="sec1">
             <nav>
@@ -50,14 +59,13 @@
                     <img src="/code/img/slide4.webp" class="banner">
                 </div>
                 <script>
-                    <!-- đoạn này định làm banner tự chuyển ảnh sau 3 giây  -->                  
-                    var slideIndex = 0;                  
-                    function showNextSlide()
-                    {
-                    
-                    }                 
+                    <!-- đoạn này định làm banner tự chuyển ảnh sau 3 giây  -->
+                    var slideIndex = 0;
+                    function showNextSlide() {
+
+                    }
                     setInterval(showNextSlide, 3000);
-                    
+
                 </script>
 
             </section>
@@ -67,6 +75,6 @@
 
         </footer>
     </div>
-</body>
+
 
 </html>
