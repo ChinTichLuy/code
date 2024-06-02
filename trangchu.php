3f64a2e87d6dc0477b213242587532b77125857b
Nguyễn Thế Huy Hoàng <hoangnthph49014@gmail.com>
2024-06-02 16:22:50 +0700

    Update trangchu.php
    fix xong banner chuyển ảnh tự động

--- trangchu.php
+++ trangchu.php
@@ -23,8 +23,7 @@
                 <input type="text" name="search">
                 <button class="btn_search" aria-label="Tìm kiếm">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="icon-search">
-                        <path
-                            d="M495,466.2L377.2,348.4c29.2-35.6,46.8-81.2,46.8-130.9C424,103.5,331.5,11,217.5,11C103.4,11,11,103.5,11,217.5  
+                        <path d="M495,466.2L377.2,348.4c29.2-35.6,46.8-81.2,46.8-130.9C424,103.5,331.5,11,217.5,11C103.4,11,11,103.5,11,217.5  
                              S103.4,424,217.5,424c49.7,0,95.2-17.5,130.8-46.7L466.1,495c8,8,20.9,8,28.9,0C503,487.1,503,474.1,495,466.2z M217.5,382.9   
                              C126.2,382.9,52,308.7,52,217.5S126.2,52,217.5,52C308.7,52,383,126.3,383,217.5S308.7,382.9,217.5,382.9z">
                         </path>
@@ -52,22 +51,25 @@
         </section>
         <main>
             <section class="secSlide">
-                <div class="banner-slide" id="bannerSlide">
-                    <img src="/code/img/slide1.webp" class="banner">
-                    <img src="/code/img/slide2.webp" class="banner">
-                    <img src="/code/img/slide3.webp" class="banner">
-                    <img src="/code/img/slide4.webp" class="banner">
+                <div class="banner-container">
+                    <img id="bannerImage" class="banner" src="/code/img/slide1.webp" width=100%>
                 </div>
                 <script>
-                    <!-- đoạn này định làm banner tự chuyển ảnh sau 3 giây  -->
-                    var slideIndex = 0;
-                    function showNextSlide() {
+                    var index = 0;
+                    var images = [
+                        '/code/img/slide1.webp',
+                        '/code/img/slide2.webp',
+                        '/code/img/slide3.webp',
+                        '/code/img/slide4.webp'
+                    ];
+                    var bannerImage = document.getElementById('bannerImage');
 
+                    function nextSlide() {
+                        index = (index + 1) % images.length;
+                        bannerImage.src = images[index];
                     }
-                    setInterval(showNextSlide, 3000);
-
+                    setInterval(nextSlide, 2000);
                 </script>
-
             </section>
 
         </main>
